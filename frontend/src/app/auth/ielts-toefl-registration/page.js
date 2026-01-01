f86a2889c115e592d8adda79304f1efdbea9d442
Faysal-Maxamed <pheymoha17@gmail.com>
2026-01-01 11:14:11 +0300

    ..

--- frontend/src/app/auth/ielts-toefl-registration/page.js
+++ frontend/src/app/auth/ielts-toefl-registration/page.js
@@ -1,16 +1,5 @@
-import Header from "@/components/Header";
 import IELTSTOEFLRegistrationPage from "@/components/IELTSTOEFLRegistrationPage";
-import Footer from "@/components/Footer";
 
 export default function IELTSTOEFLRegistrationRoute() {
-  return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <main className="flex-grow">
-        <IELTSTOEFLRegistrationPage />
-      </main>
-      <Footer />
-    </div>
-  );
+  return <IELTSTOEFLRegistrationPage />;
 }
-
